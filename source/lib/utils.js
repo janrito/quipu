2c3281c93257b541a8b86457f1441950c6da27e6
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2021-04-29 23:30:13 +0100

    docstrings in utils

--- source/lib/utils.js
+++ source/lib/utils.js
@@ -1,3 +1,6 @@
+/**
+ * Attempts to parse JSON from a string, otherwise returns false
+ */
 export const tryParseJSON = jsonString => {
   try {
     const o = JSON.parse(jsonString);
@@ -11,6 +14,11 @@ export const tryParseJSON = jsonString => {
   return false;
 };
 
+/**
+ * Encodes keys and values to be passed as URL parameters as expected by
+ * the pinboard API.
+ * If multiple values in a key, they will be joined by a `+`
+ */
 export const encodeParameters = parameters =>
   Object.entries(parameters)
     .map(([key, value]) => {
@@ -24,6 +32,10 @@ export const encodeParameters = parameters =>
 
 export const randomSuffix = () => Math.round(Math.random() * 100000);
 
+/**
+ * Transforms a browser tab as returned from the `browser.tabs`
+ * interface object to a bookmark object as expected by the pinboard API
+ */
 export const browserTabToBookmark = browserTab => ({
   description: browserTab.title,
   href: browserTab.url,
