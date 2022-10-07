fb34d54e4d1eadfa25751a9cc838e39d7e1d0ad9
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-10-07 23:47:10 +0100

    update dependencies
    - get rid of autoprefixer
    - update stylelint config

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "postcss-custom-properties": {},
     "postcss-fontpath": { checkFiles: true, ie8Fix: true },
     tailwindcss: "tailwind.config.js",
-    autoprefixer: {},
     "postcss-nested": {},
   },
   ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
