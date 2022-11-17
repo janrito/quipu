82babefc32db64f5e05e391fccde3a81a4778d02
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-11-17 15:23:54 +0000

    space to save half-life and exceptions settings

--- source/lib/options-storage.js
+++ source/lib/options-storage.js
@@ -5,6 +5,8 @@ export default new OptionsSync({
     pinboardAPIToken: null,
     pinboardRootTag: ".quipu",
     pages: [{ name: "Home", cards: ["Clipboard", "to-read"] }],
+    tabDecayHalfLife: 30 * 24 * 60 * 60 * 1000,
+    tabDecayExceptions: [],
   },
   migrations: [OptionsSync.migrations.removeUnused],
   logging: true,
