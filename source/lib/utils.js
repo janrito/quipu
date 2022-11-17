2135cc4ceb866d2c8d6594ec9159bc810b6dc479
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-11-17 15:23:23 +0000

    Utilities to calculate decay and format a half-life

--- source/lib/utils.js
+++ source/lib/utils.js
@@ -57,3 +57,51 @@ export const modifyElementClasses = (element, add = [], remove = []) => {
   ].join(" ");
   return element;
 };
+
+/**
+ * Pretty format a half life
+ */
+export const formatHalfLife = halfLifeInMilliseconds => {
+  const halfLifeWeeks = Math.floor(halfLifeInMilliseconds / (1000 * 60 * 60 * 24 * 7));
+  const halfLifeDays = Math.floor(
+    halfLifeInMilliseconds / (1000 * 60 * 60 * 24) - halfLifeWeeks * 7
+  );
+  const halfLifeHours = Math.floor(
+    halfLifeInMilliseconds / (1000 * 60 * 60) - (halfLifeWeeks * 24 * 7 + halfLifeDays * 24)
+  );
+  const halfLifeMinutes = Math.floor(
+    halfLifeInMilliseconds / (1000 * 60) -
+      (halfLifeWeeks * 60 * 24 * 7 + halfLifeDays * 60 * 24 + halfLifeHours * 60)
+  );
+  const halfLifeSeconds = Math.floor(
+    halfLifeInMilliseconds / 1000 -
+      (halfLifeWeeks * 60 * 60 * 24 * 7 +
+        halfLifeDays * 60 * 60 * 24 +
+        halfLifeHours * 60 * 60 +
+        halfLifeMinutes * 60)
+  );
+
+  return [
+    halfLifeWeeks ? `${halfLifeWeeks} week${halfLifeWeeks > 1 ? "s" : ""}` : "",
+    halfLifeDays ? `${halfLifeDays} day${halfLifeDays > 1 ? "s" : ""}` : "",
+    halfLifeHours ? `${halfLifeHours} hour${halfLifeHours > 1 ? "s" : ""}` : "",
+    halfLifeMinutes ? `${halfLifeMinutes} minute${halfLifeMinutes > 1 ? "s" : ""}` : "",
+    halfLifeSeconds ? `${halfLifeSeconds} second${halfLifeSeconds > 1 ? "s" : ""}` : "",
+  ]
+    .filter(a => a)
+    .join(", ");
+};
+
+/**
+ * Calculates the probability of being alive of an object based on it's lifetime and
+ * half life
+ * e.g. the probability of being alive with lifetime of 20, and a half-life of 20
+ * should be 50%
+ */
+export const pAlive = (lifetime, halfLife) => 2 ** (-lifetime / halfLife);
+
+/**
+ * Samples whether to kill an object based on the probability of being alive
+ * from a uniform distribution
+ */
+export const shouldKill = (lifetime, halfLife) => Math.random() > pAlive(lifetime, halfLife);
