1ce26c7f6b5aa95a517f39ea49fed2cabdb513f3
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-11-17 15:21:58 +0000

    Make local version have a pink banner so that I can tell the difference

--- source/App.svelte
+++ source/App.svelte
@@ -16,18 +16,18 @@ const toggleSettings = () => {
 </script>
 
 <div class="flex flex-col h-screen w-screen justify-between">
-  <header class="h-7 bg-gray-800 border-b border-gray-600 py-1 px-3">
+  <header class="h-7 bg-head-800 border-b border-head-600 py-1 px-3">
     <div class="flex flex-row h-full">
       <div class="h-full">
-        <h1 class="ml-7 text-xs font-extralight text-gray-400">quipu</h1>
+        <h1 class="ml-7 text-xs font-extralight text-head-400">quipu</h1>
       </div>
       <div class="h-full flex-grow">
-        <p class="ml-3 text-xs font-extralight text-gray-500">
+        <p class="ml-3 text-xs font-extralight text-head-500">
           <a class="" href="https://pinboard.in/" target="_blank" rel="noreferrer">pinboard</a>
         </p>
       </div>
       <div class="flex-none w-56 h-full">
-        <p class="ml-5 text-xs font-extralight text-gray-500">
+        <p class="ml-5 text-xs font-extralight text-head-500">
           <a href="#settings" on:click|preventDefault="{toggleSettings}">settings</a>
         </p>
       </div>
@@ -43,7 +43,7 @@ const toggleSettings = () => {
       <div class="flex-none bg-gray-100 w-56 p-2 h-full"><Tabs /></div>
     </div>
   </main>
-  <footer class="h-2 bg-gray-800 border-t border-gray-600 py-1 px-3">
+  <footer class="h-2 bg-head-800 border-t border-head-600 py-1 px-3">
     <div class="flex flex-row h-full"></div>
   </footer>
 </div>
