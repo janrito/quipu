96f27806f28406a9391f33bcc0ac69cf4b1d2a64
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-11-19 23:45:04 +0000

    constants

---
+++ source/lib/constants.js
@@ -0,0 +1,5 @@
+export const BROWSER_TAB_PREFIX = "tab";
+
+export const TAB_QUERY = {
+  url: ["http://*/*", "https://*/*", "ws://*/*", "wss://*/*"],
+};
--- source/stores/browser-tabs.js
+++ source/stores/browser-tabs.js
@@ -2,16 +2,14 @@ import browser from "webextension-polyfill";
 import throttle from "lodash/throttle";
 import { readable } from "svelte/store";
 
-const BROWSER_TAB_PREFIX = "tab";
+import { BROWSER_TAB_PREFIX, TAB_QUERY } from "../lib/constants";
 
 export default readable([], set => {
   const eventTypes = ["onAttached", "onDetached", "onMoved", "onRemoved", "onUpdated"];
   const updateTabs = throttle(
     async () => {
       const tabs = await browser.tabs
-        .query({
-          url: ["http://*/*", "https://*/*", "ws://*/*", "wss://*/*"],
-        })
+        .query(TAB_QUERY)
         .then(tabs =>
           tabs.map((tab, idx) => ({ ...tab, id: `${BROWSER_TAB_PREFIX}-${idx}`, _id: tab.id }))
         );
