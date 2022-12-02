dddb5336d4d08cab3fbce36b811c7208bdbd701c
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2022-12-02 18:00:40 +0000

    make tabLifetimes a store, hopefully this fixes concurrency issues

--- source/background.js
+++ source/background.js
@@ -1,4 +1,5 @@
-import { get } from "svelte/store";
+import debounce from "lodash/debounce";
+import { get, writable } from "svelte/store";
 import browser from "webextension-polyfill";
 
 import { MAX_DELAY_TO_SCHEDULE, TAB_QUERY, UPDATED_SETTINGS_EVENT } from "./lib/constants";
@@ -7,82 +8,101 @@ import { closeTab, compileValidURLPatterns, findURLPattern, sampleLifetime } fro
 import decayedTabs from "./stores/decayed-tabs";
 import settings from "./stores/settings";
 
-const tabLifetimes = {};
+const tabLifetimes = writable({});
 
-const decayTab = tabId => {
+const decayTab = debounce(tabId => {
   browser.tabs.get(tabId).then(tab => {
     decayedTabs.add(tab);
     closeTab(tabId);
+    tabLifetimes.update(currentTabLifetimes => {
+      if (currentTabLifetimes[tabId]) {
+        delete currentTabLifetimes[tabId];
+      }
+      return currentTabLifetimes;
+    });
   });
+}, 100);
+
+const clearTabLifetimes = (tabIds, currentTabLifetimes) => {
+  const tabIdsSet = new Set(tabIds);
+  tabIdsSet.forEach(tabId => {
+    const { timerId } = currentTabLifetimes[tabId];
+    clearTimeout(timerId);
+  });
+  return Object.fromEntries(Object.entries(currentTabLifetimes).filter(key => !tabIdsSet.has(key)));
 };
 
-const clearTabLifetime = tabId => {
-  const { timerId } = tabLifetimes[tabId];
-  clearTimeout(timerId);
-  delete tabLifetimes[tabId];
-};
-
-const setNewTabLifetime = ({ tab, tabDecayExceptions, tabDecayHalfLife }) => {
+const setNewTabLifetime = (tab, currentTabLifetimes, { tabDecayExceptions, tabDecayHalfLife }) => {
   const matchingExceptionPattern = findURLPattern(tab.url, tabDecayExceptions);
 
   if (tab.pinned || matchingExceptionPattern) {
-    return;
+    return currentTabLifetimes;
   }
   const now = new Date().valueOf();
   const lastAccessed = tab.lastAccessed.valueOf();
-  const { lifetime } = tabLifetimes[tab.id] || { lifetime: sampleLifetime(tabDecayHalfLife) };
+  const { lifetime } = currentTabLifetimes[tab.id] || {
+    lifetime: sampleLifetime(tabDecayHalfLife),
+  };
   const currentLifeSpan = now - lastAccessed;
   const delay = lifetime - currentLifeSpan > 0 ? lifetime - currentLifeSpan : 0;
 
   if (delay > MAX_DELAY_TO_SCHEDULE) {
     // only set the decay timer on tabs that are likely to decay soon.
-    return;
+    return currentTabLifetimes;
   }
   const timerId = setTimeout(decayTab, delay, tab.id);
-  tabLifetimes[tab.id] = { timerId, lifetime };
+  currentTabLifetimes[tab.id] = { timerId, lifetime };
+  return currentTabLifetimes;
 };
 
-const updateTabLifetimes = async (forceOn = [], forceOnAll = false) => {
+const updateTabLifetimes = debounce(async (forceOn = [], forceOnAll = false) => {
   await settings.read();
   const currentSettings = get(settings);
   const tabDecayHalfLife = currentSettings.tabDecayHalfLife;
   const tabDecayExceptions = compileValidURLPatterns(currentSettings.tabDecayExceptions);
-
   const forceOnSet = new Set(forceOn);
+
+  let currentTabLifetimes = get(tabLifetimes);
 
   browser.tabs.query(TAB_QUERY).then(tabs => {
     const tabIds = new Set([]);
     tabs.map(tab => {
+      // keep track of all existing tabs
       tabIds.add(tab.id);
-      const isSet = tabLifetimes[tab.id] !== undefined;
+
+      const isSet = currentTabLifetimes[tab.id] !== undefined;
 
       if (isSet && tab.active) {
         // If active on window, clear it and return
-        clearTabLifetime(tab.id);
+        currentTabLifetimes = clearTabLifetimes([tab.id], currentTabLifetimes);
         return;
       }
 
       if (isSet && (forceOnSet.has(tab.id) || forceOnAll)) {
         // clear all tabs in force list
-        clearTabLifetime(tab.id);
+        currentTabLifetimes = clearTabLifetimes([tab.id], currentTabLifetimes);
       }
 
       if (tabDecayHalfLife <= 0) {
         return;
       }
       // define new lifetime for all unset ids
-      setNewTabLifetime({ tab, tabDecayHalfLife, tabDecayExceptions });
+      currentTabLifetimes = setNewTabLifetime(tab, currentTabLifetimes, {
+        tabDecayHalfLife,
+        tabDecayExceptions,
+      });
     });
 
-    Object.keys(tabLifetimes).forEach(tabId => {
-      // clear lifetimes for tabs that don't exist any more
-      // – for example those closed manually
-      if (~tabIds.has(tabId)) {
-        clearTabLifetime(tabId);
-      }
-    });
+    // clear lifetimes for tabs that don't exist any more
+    // – for example those closed manually
+    currentTabLifetimes = clearTabLifetimes(
+      Object.keys(currentTabLifetimes).filter(tabId => !tabIds.has(Number(tabId))),
+      currentTabLifetimes
+    );
+
+    tabLifetimes.set(currentTabLifetimes);
   });
-};
+}, 5000);
 
 const onActivatedHandler = ({ previousTabId }) => {
   updateTabLifetimes([previousTabId]);
