103068c20409a65bf6e5a9aeafd6c516e5e17291
Alejandro Giacometti <alejandro.giacometti@beauhurst.com>
2025-02-27 11:07:15 +0000

    modernise bookmarkeditor

--- source/components/BookmarkEditor.svelte
+++ source/components/BookmarkEditor.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-import { createEventDispatcher } from "svelte";
-
+import type { BookmarkSchema } from "../lib/types.js";
 import IconDelete from "./IconDelete.svelte";
 
 interface Props {
-  description: any;
-  href: any;
-  tags?: any;
+  description: string;
+  href: URL;
+  tags?: string[];
   extended?: string;
-  parentTags?: any;
-  cardsInPage?: any;
+  parentTags?: string[];
+  cardsInPage?: string[];
+  updateBookmark: (bookmark: BookmarkSchema) => void;
+  deleteBookmark: (href: URL) => void;
+  close: () => void;
 }
 
 let {
@@ -19,36 +21,64 @@ let {
   extended = $bindable(""),
   parentTags = [],
   cardsInPage = [],
+  updateBookmark,
+  deleteBookmark,
+  close,
 }: Props = $props();
 
-const dispatch = createEventDispatcher();
+type tagType = "cardTag" | "leafTag" | "parentTag";
 
-const onDone = () => {
-  dispatch("done");
+const pickTagType = (tag: string): tagType => {
+  if (parentTags.includes(tag)) {
+    return "parentTag";
+  } else if (cardsInPage.includes(tag)) {
+    return "cardTag";
+  }
+  return "leafTag";
 };
 
-const onSave = () => {
+let tagsToDraw = $derived(
+  tags
+    .map(tag => ({ name: tag, type: pickTagType(tag) }))
+    .sort((a, b) =>
+      a.type === b.type
+        ? a.name.localeCompare(b.name)
+        : typeOrder.indexOf(a.type) - typeOrder.indexOf(b.type)
+    )
+);
+
+const onDone = (event: Event) => {
+  event.preventDefault();
+  close();
+};
+
+const onSave = (event: Event) => {
+  event.preventDefault();
   // actually save values
-  dispatch("update", {
+  updateBookmark({
     href,
     description,
     extended,
     tags,
-  });
-  dispatch("done");
+  } as BookmarkSchema);
+  close();
 };
 
-const onDelete = () => {
-  dispatch("delete", { href });
-  dispatch("done");
+const onDelete = (event: Event) => {
+  event.preventDefault();
+  deleteBookmark(href);
+  close();
 };
 
-const updateTags = event => {
-  tags = [...new Set(event.target.textContent.trim().split(/\s+/))];
+const updateTags = (event: Event) => {
+  if (!event.target || !(event.target instanceof HTMLElement)) {
+    return;
+  }
+  tags = [...new Set(event.target.textContent?.trim().split(/\s+/) || [])];
 };
 
-const typeOrder = ["leafTag", "cardTag", "parentTag"];
-const tagStyle = {
+const typeOrder: tagType[] = ["leafTag", "cardTag", "parentTag"];
+const tagStyle: Record<tagType, string> = {
   parentTag:
     "border-blue-400 bg-blue-50 text-blue-500 dark:border-blue-500 dark:bg-blue-900 dark:text-blue-400",
   cardTag:
@@ -56,23 +86,6 @@ const tagStyle = {
   leafTag:
     "border-yellow-400 bg-yellow-50 text-yellow-500 dark:border-yellow-500 dark:bg-yellow-900 dark:text-yellow-400",
 };
-
-let tagsToDraw = $derived(
-  tags
-    .map(tag => ({
-      name: tag,
-      type: parentTags.includes(tag)
-        ? "parentTag"
-        : cardsInPage.includes(tag)
-          ? "cardTag"
-          : "leafTag",
-    }))
-    .sort((a, b) =>
-      a.type === b.type
-        ? a.name.localeCompare(b.name)
-        : typeOrder.indexOf(a.type) - typeOrder.indexOf(b.type)
-    )
-);
 </script>
 
 <div class="p-2">
@@ -83,7 +96,7 @@ let tagsToDraw = $derived(
     </h1>
 
     <p
-      class="mb-2 break-all border-b-2 border-pink-300 bg-pink-100 text-sm font-extralight text-pink-400 dark:border-pink-600 dark:bg-pink-800 dark:text-pink-500">
+      class="mb-2 border-b-2 border-pink-300 bg-pink-100 text-sm font-extralight break-all text-pink-400 dark:border-pink-600 dark:bg-pink-800 dark:text-pink-500">
       {href}
     </p>
 
@@ -97,7 +110,7 @@ let tagsToDraw = $derived(
     <p
       class="mb-2 border-b-2 border-gray-400 bg-gray-100 text-gray-500 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-400"
       contenteditable="true"
-      onkeyup={updateTags}>
+      onblur={updateTags}>
       {tags.join(" ")}
     </p>
 
@@ -114,14 +127,14 @@ let tagsToDraw = $derived(
   </div>
   <div class="flex flex-row justify-end">
     <button
-      class="max-w-1/3 my-2 mr-auto border-b-2 border-red-500 bg-red-200 px-1.5 text-red-500 hover:border-red-200 hover:bg-red-500 hover:text-red-200 dark:border-red-400 dark:bg-red-700 dark:text-red-400 dark:hover:border-red-700 dark:hover:bg-red-400 dark:hover:text-red-700"
+      class="my-2 mr-auto max-w-1/3 border-b-2 border-red-500 bg-red-200 px-1.5 text-red-500 hover:border-red-200 hover:bg-red-500 hover:text-red-200 dark:border-red-400 dark:bg-red-700 dark:text-red-400 dark:hover:border-red-700 dark:hover:bg-red-400 dark:hover:text-red-700"
       onclick={onDelete}
       >delete <span class="inline-block align-text-bottom"><IconDelete /></span></button>
     <button
-      class="max-w-1/3 my-2 ml-2 border-b-2 border-gray-500 bg-gray-200 px-1.5 text-gray-500 hover:border-gray-200 hover:bg-gray-500 hover:text-gray-200 dark:border-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-400 dark:hover:text-gray-700"
+      class="my-2 ml-2 max-w-1/3 border-b-2 border-gray-500 bg-gray-200 px-1.5 text-gray-500 hover:border-gray-200 hover:bg-gray-500 hover:text-gray-200 dark:border-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-400 dark:hover:text-gray-700"
       onclick={onDone}>cancel</button>
     <button
-      class="max-w-1/3 my-2 ml-2 border-b-2 border-blue-500 bg-blue-200 px-1.5 text-blue-500 hover:border-blue-200 hover:bg-blue-500 hover:text-blue-200 dark:border-blue-400 dark:bg-blue-700 dark:text-blue-400 dark:hover:border-blue-700 dark:hover:bg-blue-400 dark:hover:text-blue-700"
+      class="my-2 ml-2 max-w-1/3 border-b-2 border-blue-500 bg-blue-200 px-1.5 text-blue-500 hover:border-blue-200 hover:bg-blue-500 hover:text-blue-200 dark:border-blue-400 dark:bg-blue-700 dark:text-blue-400 dark:hover:border-blue-700 dark:hover:bg-blue-400 dark:hover:text-blue-700"
       onclick={onSave}>save</button>
   </div>
 </div>
