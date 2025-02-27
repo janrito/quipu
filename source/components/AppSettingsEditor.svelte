e494b0c47c20c04882b13588d5a99dc30effc6d5
Alejandro Giacometti <alejandro.giacometti@beauhurst.com>
2025-02-27 10:29:29 +0000

    modernise appsettings

--- source/components/AppSettingsEditor.svelte
+++ source/components/AppSettingsEditor.svelte
@@ -1,38 +1,45 @@
-<script>
-import throttle from "lodash/throttle";
-import { createEventDispatcher } from "svelte";
-import { preventDefault } from "svelte/legacy";
+<script lang="ts">
+import { throttle } from "lodash";
 
-import { clearEntireCache } from "../lib/cache";
+import { clearEntireCache } from "../lib/cache.js";
 import {
   compileURLPattern,
   compileValidURLPatterns,
   findURLPattern,
   formatTimeDelta,
-  tryParseJSON,
-} from "../lib/utils";
-import appSettings from "../stores/app-settings";
-import browserTabs from "../stores/browser-tabs";
+  tryParseJSONSettings,
+} from "../lib/utils.js";
+import appSettings from "../stores/app-settings.js";
+import browserTabs from "../stores/browser-tabs.js";
 import IconDelete from "./IconDelete.svelte";
 import UITabs from "./UITabs.svelte";
 
-const dispatch = createEventDispatcher(),
-  tabs = [
-    { id: 0, name: "pinboard" },
-    { id: 1, name: "tab decay" },
-    { id: 2, name: "raw" },
-    { id: 3, name: "cache" },
-  ];
+interface Props {
+  closeEditor: () => void;
+}
 
-let currentTabId = $state(tabs[0].id);
+let { closeEditor }: Props = $props();
 
-const goToTab = event => {
-  currentTabId = Number(event.detail.id);
-};
+const tabs = ["pinboard", "tab decay", "raw", "cache"];
+
+let currentTabDecayHalfLife = $state($appSettings.tabDecayHalfLife);
+
+let currentTab = $state(tabs[0]);
+let tabsMatchingLiveExceptions = $derived(
+  $browserTabs
+    .flat()
+    .map(tab =>
+      tab.href ? findURLPattern(tab.href, currentTabDecayExemptions) && tab.href : undefined
+    )
+    .filter(url => url)
+);
 
 const updateAllAppSettings = throttle(
-  async event => {
-    const newAppSettings = tryParseJSON(event.target.textContent);
+  async (event: Event) => {
+    if (!event.target || !(event.target instanceof HTMLElement)) {
+      return;
+    }
+    const newAppSettings = tryParseJSONSettings(event.target.textContent || "");
     if (newAppSettings) {
       $appSettings = newAppSettings;
     }
@@ -41,21 +48,27 @@ const updateAllAppSettings = throttle(
   { trailing: true }
 );
 
-let currentTabDecayHalfLife = $state($appSettings.tabDecayHalfLife);
 const updateTabDecayHalfLife = () => {
   $appSettings.tabDecayHalfLife = currentTabDecayHalfLife;
 };
 
-const updateTabDecayExemptions = async event => {
+const updateTabDecayExemptions = async (event: Event) => {
+  if (!event.target || !(event.target instanceof HTMLElement)) {
+    return;
+  }
   const newTabDecayExemptions = event.target.innerText
     .split("\n")
     .map(line => {
       const trimmed = line.trim();
-      if (!trimmed) {
-        return false;
-      }
+      if (!trimmed) return "";
 
-      return compileURLPattern(trimmed) || trimmed.startsWith("//") ? trimmed : `// ${line}`;
+      const compiledURLPattern = compileURLPattern(trimmed);
+
+      // return compiled pattern if it is valid
+      if (compiledURLPattern) return String(compiledURLPattern);
+
+      // return commented line if it is invalid
+      return trimmed.startsWith("// ") ? trimmed : `// INVALID: ${line}`;
     })
     .filter(line => line)
     .sort();
@@ -63,39 +76,35 @@ const updateTabDecayExemptions = async event => {
   if (newTabDecayExemptions) {
     $appSettings = { ...$appSettings, tabDecayExceptions: [...new Set(newTabDecayExemptions)] };
   }
-  tabDecayExceptionsEditMode = false;
 };
 
 let currentTabDecayExemptions = $state(compileValidURLPatterns($appSettings.tabDecayExceptions));
 
 const liveReloadExceptionMatches = throttle(
-  async event => {
+  async (event: Event) => {
+    if (!event.target || !(event.target instanceof HTMLElement)) {
+      return;
+    }
     currentTabDecayExemptions = compileValidURLPatterns(event.target.innerText.split("\n"));
   },
   100,
   { trailing: true }
 );
 
-const clearCacheHandler = async () => {
+const clearCacheHandler = async (event: Event) => {
+  event.preventDefault();
   await clearEntireCache();
 };
-
-const closeAppSettings = () => {
-  dispatch("close");
+const handleCloseEditor = (event: Event) => {
+  event.preventDefault();
+  closeEditor();
 };
-
-let currentTab = $derived(tabs.find(tab => tab.id === currentTabId));
-let tabsMatchingLiveExceptions = $derived(
-  $browserTabs
-    .map(tab => findURLPattern(tab.url, currentTabDecayExemptions) && tab.url)
-    .filter(url => url)
-);
 </script>
 
-<UITabs {tabs} selectedTabId={currentTabId} on:selectTab={goToTab}>
-  <div class="flex h-full flex-col overflow-y-auto overflow-x-hidden pr-3">
+<UITabs {tabs} bind:selectedTab={currentTab}>
+  <div class="flex h-full flex-col overflow-x-hidden overflow-y-auto pr-3">
     <div class="flex flex-row flex-wrap">
-      {#if currentTab.name === "pinboard"}
+      {#if currentTab === "pinboard"}
         <label id="pinboard-api-token" for="pinboardAPIToken" class="mt-5 w-1/2 p-1 pl-7">
           <span>Pinboard API Token</span>
           <input
@@ -121,7 +130,7 @@ let tabsMatchingLiveExceptions = $derived(
             name="pinboardRootTag"
             bind:value={$appSettings.pinboardRootTag} />
         </label>
-      {:else if currentTab.name === "tab decay"}
+      {:else if currentTab === "tab decay"}
         <label id="tab-decay-half-life" class="mt-5 w-1/2 p-1 pl-7">
           <span>Half Life </span>
           <input
@@ -149,33 +158,33 @@ let tabsMatchingLiveExceptions = $derived(
               contenteditable="true"
               onfocusout={updateTabDecayExemptions}
               onkeyup={liveReloadExceptionMatches}
-              name="tabDecayExceptions">{$appSettings.tabDecayExceptions.join("\n")}</pre>
+              id="tabDecayExceptions">{$appSettings.tabDecayExceptions.join("\n")}</pre>
 
             <p class="text-xs">
               New line separated URLPatters to exempt from tab decay e.g. [*://*.google.*/*\?*#*]
             </p>
           </label>
-          <p class="mb-1 mt-4 text-sm">Open tabs matching exceptions</p>
+          <p class="mt-4 mb-1 text-sm">Open tabs matching exceptions</p>
           <pre
-            class="h-min-content w-full whitespace-pre-wrap border-b-2 border-gray-300 bg-gray-50 p-3 text-xs text-gray-400 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-500">{tabsMatchingLiveExceptions.join(
+            class="h-min-content w-full border-b-2 border-gray-300 bg-gray-50 p-3 text-xs whitespace-pre-wrap text-gray-400 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-500">{tabsMatchingLiveExceptions.join(
               "\n"
             )}</pre>
         </div>
-      {:else if currentTab.name === "raw"}
+      {:else if currentTab === "raw"}
         <label id="all-settings" for="allSettings" class="mt-5 w-full p-1 pl-7">
           <span>All Settings</span>
           <pre
             class="h-min-content w-full border-b-2 border-gray-300 bg-gray-100 p-3 text-xs text-gray-800 dark:border-gray-300 dark:bg-gray-900 dark:text-gray-100"
             contenteditable="true"
             onfocusout={updateAllAppSettings}
-            name="allSettings">{JSON.stringify($appSettings, null, " ")}</pre>
+            id="allSettings">{JSON.stringify($appSettings, null, " ")}</pre>
         </label>
-      {:else if currentTab.name === "cache"}
+      {:else if currentTab === "cache"}
         <div class="w-full flex-grow">
-          <p class="ml-7 mt-5">
+          <p class="mt-5 ml-7">
             <button
               class="border-b-2 border-red-500 bg-red-200 px-1.5 text-red-500 hover:border-red-200 hover:bg-red-500 hover:text-red-200 dark:border-red-400 dark:bg-red-700 dark:text-red-400 dark:hover:border-red-700 dark:hover:bg-red-400 dark:hover:text-red-700"
-              onclick={preventDefault(clearCacheHandler)}>
+              onclick={clearCacheHandler}>
               clear cache <span class="inline-block align-text-bottom"><IconDelete /></span>
             </button>
           </p>
@@ -184,8 +193,8 @@ let tabsMatchingLiveExceptions = $derived(
       <div class="w-3/4"></div>
       <div class="w-1/4 p-1">
         <button
-          class="w-full border-b-2 border-gray-500 bg-gray-200 pl-5 pr-1.5 text-left text-gray-500 hover:border-gray-200 hover:bg-gray-500 hover:text-gray-200 dark:border-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-400 dark:hover:text-gray-700"
-          onclick={preventDefault(closeAppSettings)}>
+          class="w-full border-b-2 border-gray-500 bg-gray-200 pr-1.5 pl-5 text-left text-gray-500 hover:border-gray-200 hover:bg-gray-500 hover:text-gray-200 dark:border-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-400 dark:hover:text-gray-700"
+          onclick={handleCloseEditor}>
           done
         </button>
       </div>
