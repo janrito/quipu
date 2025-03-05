042a56ee91dbdc7f26dd413269efeaed495f9129
Alejandro Giacometti <alejandro.giacometti@beauhurst.com>
2025-03-05 16:47:12 +0000

    pragmatic drag and drop for UITabs

--- src/components/UITab.svelte
+++ src/components/UITab.svelte
@@ -1,25 +1,53 @@
-<script lang="ts">
-import type { TagMap } from "~/lib/types.js";
-
-import TagEditor from "./TagEditor.svelte";
-
+<script lang="ts" module>
 interface Props {
   label: string;
+  element?: HTMLAnchorElement;
   selected?: boolean;
   editable?: boolean;
   suggestedTags?: TagMap[];
+  preview?: boolean;
   deleteTab?: () => void;
   renameTab?: (newTabName: string) => void;
 }
 
+interface DragState {
+  state: "idle" | "in-flight" | "over";
+  edge?: Edge;
+}
+</script>
+
+<script lang="ts">
+import {
+  attachClosestEdge,
+  extractClosestEdge,
+} from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge";
+import type { Edge } from "@atlaskit/pragmatic-drag-and-drop-hitbox/types";
+import {
+  draggable,
+  dropTargetForElements,
+} from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import { preserveOffsetOnSource } from "@atlaskit/pragmatic-drag-and-drop/element/preserve-offset-on-source";
+import { setCustomNativeDragPreview } from "@atlaskit/pragmatic-drag-and-drop/element/set-custom-native-drag-preview";
+import { mount, unmount } from "svelte";
+
+import type { TagMap } from "~/lib/types.js";
+
+import TagEditor from "./TagEditor.svelte";
+import UITab from "./UITab.svelte";
+
 let {
   label,
+  element = $bindable(undefined),
   selected = $bindable(false),
   editable = false,
+  preview = false,
   suggestedTags = [],
   deleteTab = () => {},
   renameTab = () => {},
 }: Props = $props();
+
+const idle: DragState = { state: "idle" };
+let dragState: DragState = $state(idle);
 
 let editMode: boolean = $state(false);
 
@@ -32,10 +60,66 @@ const handleClick = (event: Event) => {
   }
 };
 
-const tabHeadingStyle = "border-gray-300 text-gray-400 dark:border-gray-600 dark:text-gray-500";
-const selectedTabHeadingStyle =
-  "border-gray-600 text-gray-800 dark:border-gray-300 dark:text-gray-200";
+$effect(() => {
+  if (element) {
+    draggable({
+      element,
+      canDrag: () => (element && editable && !editMode ? true : false),
+      onDragStart: () => (dragState = { state: "in-flight" }),
+      onDrop: () => (dragState = idle),
+      getInitialData: () => ({ label, type: "UITab" }),
+      onGenerateDragPreview({ nativeSetDragImage, location, source }) {
+        setCustomNativeDragPreview({
+          nativeSetDragImage,
+          getOffset: preserveOffsetOnSource({
+            element: source.element,
+            input: location.current.input,
+          }),
+          render({ container }) {
+            const app = mount(UITab, {
+              target: container,
+              props: { label: label, selected: selected, preview: true },
+            });
+            return () => unmount(app);
+          },
+        });
+      },
+    });
+    dropTargetForElements({
+      element,
+      getIsSticky: () => true,
+      canDrop: ({ source }) => source.data.type === "UITab" && source.data.label !== label,
+      getData: ({ input }) =>
+        attachClosestEdge(
+          { label, type: "UITab" },
+          {
+            element: element!,
+            input,
+            allowedEdges: ["left", "right"],
+          }
+        ),
+      onDragEnter: ({ self }) => {
+        dragState = { state: "over", edge: extractClosestEdge(self.data) || undefined };
+      },
+      onDrag: ({ self }) => {
+        const closestEdge = extractClosestEdge(self.data);
+
+        if (dragState.state === "over" && closestEdge && dragState.edge !== closestEdge) {
+          dragState = { ...dragState, edge: closestEdge };
+        }
+      },
+      onDragLeave: () => (dragState = idle),
+      onDrop: () => (dragState = idle),
+    });
+  }
+});
 </script>
+
+{#snippet indicator(edge: Edge | undefined, side: Edge)}
+  {#if edge === side}
+    <div class="width-2 background-gray-500 h-full rounded-xl border-2 border-blue-300"></div>
+  {/if}
+{/snippet}
 
 {#if editable && editMode && selected}
   <TagEditor
@@ -44,9 +128,14 @@ const selectedTabHeadingStyle =
     bind:value={() => label, newName => renameTab(newName)}
     {suggestedTags} />
 {:else}
+  {@render indicator(dragState.edge, "left")}
   <a
+    bind:this={element}
     href="#page-{label}"
-    class="{selected ? selectedTabHeadingStyle : tabHeadingStyle}
-      mx-0 mt-0 -mb-0.5 truncate border-b-2 bg-white px-3 text-sm font-extralight hover:border-gray-400 dark:bg-black hover:dark:border-gray-500"
+    class:in-flight={dragState.state === "in-flight"}
+    class:selected
+    class:preview
+    class="mx-0 mt-0 truncate border-b-2 border-gray-300 bg-white px-3 pb-1 text-sm font-extralight text-gray-400 hover:border-gray-400 dark:border-gray-600 dark:bg-black dark:text-gray-500 hover:dark:border-gray-500 [&.in-flight]:opacity-40 [&.preview]:pt-1 [&.preview]:pb-2 [&.preview]:drop-shadow-lg [&.selected]:border-gray-600 [&.selected]:text-gray-800 [&.selected]:dark:border-gray-300 [&.selected]:dark:text-gray-200"
     onclick={handleClick}>{label}</a>
+  {@render indicator(dragState.edge, "right")}
 {/if}
--- src/components/UITabs.svelte
+++ src/components/UITabs.svelte
@@ -1,20 +1,4 @@
-<style>
-@reference "../assets/main.pcss";
-:global(.active-droppable-tab-target) {
-  @apply opacity-80;
-}
-</style>
-
-<script lang="ts">
-import type { Snippet } from "svelte";
-import type { DndEvent, Item } from "svelte-dnd-action";
-import { dndzone } from "svelte-dnd-action";
-
-import type { TagMap } from "~/lib/types.js";
-import { modifyElementClasses } from "~/lib/utils.js";
-
-import UITab from "./UITab.svelte";
-
+<script lang="ts" module>
 interface Props {
   tabs: string[];
   selectedTab: string;
@@ -26,6 +10,17 @@ interface Props {
   reorderTabs?(newOrder: string[]): void;
   children: Snippet;
 }
+</script>
+
+<script lang="ts">
+import { extractClosestEdge } from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge";
+import { reorderWithEdge } from "@atlaskit/pragmatic-drag-and-drop-hitbox/util/reorder-with-edge";
+import { monitorForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import type { Snippet } from "svelte";
+
+import type { TagMap } from "~/lib/types.js";
+
+import UITab from "./UITab.svelte";
 
 let {
   tabs = $bindable(),
@@ -39,12 +34,6 @@ let {
   children,
 }: Props = $props();
 
-// create in-flight tabs, set initial state to ordered tabs
-let inFlightTabs: Item[] = $state([]);
-let drawTabs = $derived(
-  inFlightTabs.length ? inFlightTabs : tabs.map((tab, index) => ({ id: index, name: tab }))
-);
-
 const deleteTabDispatcher = (tabName: string) => () => {
   if (tabs.length > 1) {
     const selectedTabIdx = tabs.findIndex(tab => tab === tabName);
@@ -53,52 +42,59 @@ const deleteTabDispatcher = (tabName: string) => () => {
   }
 };
 
-const handleReorderTabsConsider = (event: CustomEvent<DndEvent>) => {
-  inFlightTabs = event.detail.items;
-};
-
-const handleReorderTabs = (event: CustomEvent<DndEvent>) => {
-  const order = event.detail.items.map(tab => tab.name);
-  reorderTabs(order);
-  selectedTab = order[0];
-  inFlightTabs = [];
-};
-
 const handleClickCreateNewTab = (event: Event) => {
   event.preventDefault();
   createNewTab();
 };
+$effect(() => {
+  if (editable) {
+    monitorForElements({
+      canMonitor: ({ source }) => source.data.type === "UITab",
+      onDrop: ({ source, location }) => {
+        const target = location.current.dropTargets[0];
+        if (!target) {
+          return;
+        }
+        const sourceData = source.data;
+        const targetData = target.data;
 
-const styleDraggedTab = (element: HTMLElement | undefined) => {
-  if (!element) return;
-  modifyElementClasses(element, ["shadow-xl", "ring-1", "ring-gray-200"]);
-};
+        if (sourceData.type !== "UITab" || targetData.type !== "UITab") {
+          return;
+        }
+        const indexOfSource = tabs.findIndex(t => t === sourceData.label);
+        const indexOfTarget = tabs.findIndex(t => t === targetData.label);
+
+        if (indexOfTarget < 0 || indexOfSource < 0) {
+          return;
+        }
+        const closestEdgeOfTarget = extractClosestEdge(targetData);
+
+        const newOrder = reorderWithEdge({
+          list: tabs,
+          startIndex: indexOfSource,
+          indexOfTarget,
+          closestEdgeOfTarget,
+          axis: "horizontal",
+        });
+
+        reorderTabs(newOrder);
+      },
+    });
+  }
+});
 </script>
 
 <div class="flex h-full flex-col">
-  <nav class="flex h-8 flex-shrink-0 flex-row overflow-y-visible pl-5">
-    <div
-      class="grid h-7 auto-cols-max grid-flow-col gap-0 overflow-y-visible border-gray-300 dark:border-gray-600"
-      use:dndzone={{
-        items: drawTabs,
-        dropTargetStyle: {},
-        dropTargetClasses: ["active-droppable-tab-target"],
-        dropFromOthersDisabled: true,
-        transformDraggedElement: styleDraggedTab,
-        dragDisabled: !editable,
-      }}
-      onconsider={handleReorderTabsConsider}
-      onfinalize={handleReorderTabs}>
-      {#each drawTabs as tab}
+  <nav class="flexflex-shrink-0 flex-row pl-5">
+    <div class="grid auto-cols-max grid-flow-col gap-x-0 overflow-x-scroll">
+      {#each tabs as tab}
         <UITab
-          label={tab.name}
+          label={tab}
           {editable}
           {suggestedTags}
-          bind:selected={
-            () => tab.name === selectedTab, selected => selected && (selectedTab = tab.name)
-          }
-          deleteTab={deleteTabDispatcher(tab.name)}
-          renameTab={newName => renameTab(tab.name, newName)} />
+          bind:selected={() => tab === selectedTab, selected => selected && (selectedTab = tab)}
+          deleteTab={deleteTabDispatcher(tab)}
+          renameTab={newName => renameTab(tab, newName)} />
       {/each}
     </div>
 
